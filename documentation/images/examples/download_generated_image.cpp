b778986fc25eda4f776192e498ef8cf2323d75cf
Dread <42398281+D7EAD@users.noreply.github.com>
2023-01-01 12:39:04 -0500

    Create download_generated_image.cpp

---
+++ documentation/images/examples/download_generated_image.cpp
@@ -0,0 +1,21 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+OpenAI oai;
+  if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+    try {
+      Response response = oai.Image->create(
+        "a siamese cat!"
+      );
+      Network::Download(
+        "C:/some/folder/file.png",                    // to
+        response["data"][0]["url"].get<std::string>() // from
+      );
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
