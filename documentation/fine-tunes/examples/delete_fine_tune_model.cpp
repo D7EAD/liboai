26d07cd7635b0d5bd5bd5427e8d20b4f89d5eb3c
Dread <42398281+D7EAD@users.noreply.github.com>
2023-01-01 21:08:52 -0500

    Create delete_fine_tune_model.cpp

---
+++ documentation/fine-tunes/examples/delete_fine_tune_model.cpp
@@ -0,0 +1,18 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+  if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+    try {
+      Response response = oai.Model->remove(
+        "curie:ft-acmeco-2021-03-03-21-44-20"
+      );
+      std::cout << response["deleted"].get<bool>() << std::endl;
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
