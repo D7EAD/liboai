3010e4f1d2888b3e3c0afbbc52712051643ab138
Dread <42398281+D7EAD@users.noreply.github.com>
2023-01-25 00:14:12 -0500

    Update generate_edit.cpp

--- documentation/images/examples/generate_edit.cpp
+++ documentation/images/examples/generate_edit.cpp
@@ -6,14 +6,12 @@ int main() {
   OpenAI oai;
   if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
     try {
-      // call async method; returns a future
       Response response = oai.Image->create_edit(
         "C:/some/folder/otter.png",
         "A cute baby sea otter wearing a beret",
         "C:/some/folder/mask.png"
       );
 
-      // print some response data
       std::cout << response["data"][0]["url"].get<std::string>() << std::endl;
     }
     catch (std::exception& e) {
