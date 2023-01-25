3331f9922f932b4f5a106e2fd76ecdc9fbbcd352
Dread <42398281+D7EAD@users.noreply.github.com>
2023-01-24 23:57:18 -0500

    Create create_moderation_async.cpp

---
+++ documentation/moderations/examples/create_moderation_async.cpp
@@ -0,0 +1,29 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+  if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+    try {
+      // call async method; returns a future
+      auto fut = oai.Moderation->create_async(
+        "I want to kill them."
+      );
+
+      // do other work...
+
+      // check if the future is ready
+      fut.wait();
+
+      // get the contained response
+      auto response = fut.get();
+
+      // print some response data
+      std::cout << response["choices"][0]["text"] << std::endl;
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
