4162beb6dc377a55973dae762b9710dc188fdba9
Dread <42398281+D7EAD@users.noreply.github.com>
2023-01-25 00:12:04 -0500

    Update generate_edit.cpp

--- documentation/images/examples/generate_edit.cpp
+++ documentation/images/examples/generate_edit.cpp
@@ -7,9 +7,9 @@ int main() {
   if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
     try {
       Response response = oai.Image->create_edit(
-        "C:/some/folder/otter.png",
-        "C:/some/folder/mask.png",
-        "A cute baby sea otter wearing a beret"
+				"C:/some/folder/otter.png",
+				"A cute baby sea otter wearing a beret",
+        "C:/some/folder/mask.png"
       );
 
       std::cout << response["data"][0]["url"].get<std::string>() << std::endl;
