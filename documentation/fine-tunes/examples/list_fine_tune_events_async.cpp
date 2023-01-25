dc390d7ccd2cfd5fc3d0cb751918e3d55f66851a
Dread <42398281+D7EAD@users.noreply.github.com>
2023-01-25 00:46:05 -0500

    Create list_fine_tune_events_async.cpp

---
+++ documentation/fine-tunes/examples/list_fine_tune_events_async.cpp
@@ -0,0 +1,29 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+  if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+    try {
+      // call async method; returns a future
+      auto fut = oai.FineTune->list_events_async(
+        "ft-AF1WoRqd3aJAHsqc9NY7iL8F"
+      );
+
+      // do other work...
+
+      // check if the future is ready
+      fut.wait();
+
+      // get the contained response
+      auto response = fut.get();
+
+      // print some response data
+      std::cout << response["data"] << std::endl;
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
