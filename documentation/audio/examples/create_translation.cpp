609de44e69a2879c5a5138812e6f0a9fef5ae84f
Dread <42398281+D7EAD@users.noreply.github.com>
2023-03-09 12:52:41 -0500

    Create translation example

---
+++ documentation/audio/examples/create_translation.cpp
@@ -0,0 +1,19 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+  oai.auth.SetKeyEnv("OPENAI_API_KEY");
+
+  try {
+    Response res = oai.Audio->translate(
+      "C:/some/folder/file.mp3",
+      "whisper-1"
+    );
+    std::cout << res["text"] << std::endl;
+  }
+  catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+}
