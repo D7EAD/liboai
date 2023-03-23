332f4ea0082c5cb7e66aadb3444586808a22a3c1
Dread <42398281+D7EAD@users.noreply.github.com>
2023-03-22 18:06:33 -0700

    Add ifdef check for Apple machines

--- liboai/include/core/netimpl.h
+++ liboai/include/core/netimpl.h
@@ -1,27 +1,6 @@
 #pragma once
 
 /*
-	Copyright (c) 2017-2021 Huu Nguyen
-	Copyright (c) 2022 libcpr and many other contributors
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
-
 	netimpl.h : Holds the internal network control-flow implementation.
 		This header file provides the internal interface(s) used to
 		allow files such as network.h to properly work. It contains
@@ -32,7 +11,7 @@
 		cURL for People (CPR).
 */
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__APPLE__)
 	#define LIBOAI_EXPORT
 #else
 	#define LIBOAI_EXPORT __declspec(dllexport)
