b7c101e1479426c08a5b693ac9f4ffc2b4c04bb9
Dread <42398281+D7EAD@users.noreply.github.com>
2023-06-11 18:40:12 -0400

    Create get_generated_image_async.cpp

---
+++ documentation/azure/examples/get_generated_image_async.cpp
@@ -0,0 +1,30 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+
+  if (oai.auth.SetAzureKeyEnv("AZURE_API_KEY")) {
+    try {
+      auto fut = oai.Azure->get_generated_image_async(
+        "resource", "api_version",
+        "f508bcf2-e651-4b4b-85a7-58ad77981ffa"
+      );
+
+      // do other work
+
+      // wait for the future to complete
+      fut.wait();
+
+      // get the result
+      auto res = fut.get();
+
+      // output the response
+      std::cout << res << std::endl;
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
