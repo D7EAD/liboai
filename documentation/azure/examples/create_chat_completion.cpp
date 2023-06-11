dbeac320572e15b4a5ef0e71e83e72c6f323f2c0
Dread <42398281+D7EAD@users.noreply.github.com>
2023-06-11 18:12:39 -0400

    Create create_chat_completion.cpp

---
+++ documentation/azure/examples/create_chat_completion.cpp
@@ -0,0 +1,28 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+
+  Conversation convo;
+  convo.AddUserData("Hi, how are you?");
+
+  if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+    try {
+      Response res = oai.Azure->create_chat_completion(
+        "resource", "deploymentID", "api_version",
+        convo
+      );
+
+      // update the conversation with the response
+      convo.Update(res);
+
+      // print the response from the API
+      std::cout << convo.GetLastResponse() << std::endl;
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
