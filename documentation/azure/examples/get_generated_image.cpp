57d002af6c9db74eea6c6681768f3fc89103394e
Dread <42398281+D7EAD@users.noreply.github.com>
2023-06-11 18:38:24 -0400

    Create get_generated_image.cpp

---
+++ documentation/azure/examples/get_generated_image.cpp
@@ -0,0 +1,22 @@
+#include "liboai.h"
+
+using namespace liboai;
+
+int main() {
+  OpenAI oai;
+
+  if (oai.auth.SetAzureKeyEnv("AZURE_API_KEY")) {
+    try {
+      Response res = oai.Azure->get_generated_image(
+        "resource", "api_version",
+        "f508bcf2-e651-4b4b-85a7-58ad77981ffa"
+      );
+
+      // output the response
+      std::cout << res << std::endl;
+    }
+    catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
