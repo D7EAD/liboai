aa6d67d7afb7e97c4d56663434aabdb3bcd3f28d
sunjiayu <767978963@qq.com>
2024-05-29 21:07:48 +0800

    add speech

---
+++ documentation/audio/examples/create_speech_async.cpp
@@ -0,0 +1,33 @@
+#include "liboai/include/liboai.h"
+
+using namespace liboai;
+
+
+int main() {
+  OpenAI oai;
+
+  if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+    try {
+      std::ofstream ocout("demo.mp3", std::ios::binary);
+      auto fut = oai.Audio->speech_async(
+        "tts-1",
+        "alloy",
+        "Today is a wonderful day to build something people love!"
+      );
+      // do other work...
+
+      // check if the future is ready
+      fut.wait();
+
+      // get the contained response
+      auto res = fut.get();
+
+      ocout << res.content;
+      ocout.close();
+      std::cout << res.content.size() << std::endl;
+    }
+    catch (const std::exception& e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+}
